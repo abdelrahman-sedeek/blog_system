206749bd0398db19a69954bb89c0c21e33f03eea
abdelrahman-sedeek <67134107+abdelrahman-sedeek@users.noreply.github.com>
2024-06-06 23:00:56 +0300

    handling validation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,8 @@ use App\Models\post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
 
 class PostController extends Controller
 {
@@ -15,18 +17,21 @@ class PostController extends Controller
         return response()->json($posts);
     }
 
-    public function store(Request $request)
+    public function store(StorePostRequest $request)
     {
-        $request->validate([
-            'title' => 'required|string|max:255',
-            'content' => 'required|string',
-            'category_id' => 'required|exists:categories,id',
-        ]);
+
+        try{
 
         $post = $request->user()->posts()->create($request->all());
         Log::info('Post created', ['post' => $post]);
-
         return response()->json($post, 201);
+
+        }
+
+        catch (\Exception $e) {
+
+            return response()->json(['error' =>$e->getMessage()], 500);
+        }
     }
 
     public function show(Post $post)
@@ -34,18 +39,20 @@ class PostController extends Controller
         return response()->json($post);
     }
 
-    public function update(Request $request, Post $post)
+    public function update(UpdatePostRequest $request, Post $post)
     {
-        $request->validate([
-            'title' => 'sometimes|string|max:255',
-            'body' => 'sometimes|string',
-            'category_id' => 'sometimes|exists:categories,id',
-        ]);
 
-        $post->update($request->all());
-        Log::info('Post updated', ['post' => $post]);
+        try{
+            $post->update($request->all());
+            Log::info('Post updated', ['post' => $post]);
 
-        return response()->json($post);
+            return response()->json($post);
+        }
+        catch (\Exception $e) {
+
+            return response()->json(['error' =>$e->getMessage()], 500);
+        }
+
     }
 
     public function destroy(Post $post)
